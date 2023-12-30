08199f5a17edb8e0f9e81f1847438b61c3e5c4e8
gab <gabriele.rigo@gmail.com>
2023-12-30 17:02:04 +0100

    update copy

--- contracts/zero-ex/contracts/src/features/multiplex/BatchMultiplexFeature.sol
+++ contracts/zero-ex/contracts/src/features/multiplex/BatchMultiplexFeature.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 /*
-  Copyright 2024 ZeroEx Intl., RigoBlock Intl.
+  Copyright 2024 ZeroEx Intl., Rigo Intl.
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
